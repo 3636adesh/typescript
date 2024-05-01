80d38f229aa95c0afe6db41a3545fe2b177268e1
Adesh Malunjkar <adesh.malunjkar@smartdocs.ai>
2024-05-01 14:39:10 +0530

    ..

---
+++ basics/app.ts
@@ -0,0 +1,8 @@
+console.log('Hello, world');
+
+let num;  // not need initializing immediately
+num = 10;  // we can change the value of num
+
+let Num = 10;  // case insensitive
+
+const str = 'Hello, world';  // initializing immediately  , we can't change the value of strf
